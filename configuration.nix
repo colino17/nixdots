0b08da9471935e18159489e3d95ac1c00a330ed7
colino17 <51090962+colino17@users.noreply.github.com>
2022-03-31 00:11:02 -0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -43,21 +43,61 @@
 ######################
   services.xserver.desktopManager.gnome = {
     extraGSettingsOverrides = ''
-      # Change default background
       [org.gnome.desktop.background]
       picture-uri='file://${pkgs.nixos-artwork.wallpapers.mosaic-blue.gnomeFilePath}'
+     
+      [org.gnome.settings-daemon.plugins.media-keys]
+      custom-keybindings=['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/' '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/' '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/' '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/' '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4/' '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom5/']
 
-      # Favorite apps in gnome-shell
+      [org.gnome.settings-daemon.plugins.media-keys.custom-keybindings.custom0]
+      binding='<Super>t'
+      command='gnome-terminal'
+      name='open-terminal' 
+      
+      [org.gnome.settings-daemon.plugins.media-keys.custom-keybindings.custom1]
+      binding='<Super>e'
+      command='nautilus'
+      name='open-files'
+      
+      [org.gnome.settings-daemon.plugins.media-keys.custom-keybindings.custom2]
+      binding='<Super>w'
+      command='epiphany'
+      name='open-web'
+      
+      [org.gnome.settings-daemon.plugins.media-keys.custom-keybindings.custom3]
+      binding='<Super>d'
+      command='discord'
+      name='open-discord'
+      
+      [org.gnome.settings-daemon.plugins.media-keys.custom-keybindings.custom4]
+      binding='<Super>g'
+      command='gimp'
+      name='open-gimp'
+      
+      [org.gnome.settings-daemon.plugins.media-keys.custom-keybindings.custom5]
+      binding='<Super>m'
+      command='gnome-terminal --maximize -- bash -c cmatrix -bas'
+      name='enter-matrix'
+      
       [org.gnome.shell]
       favorite-apps=['org.gnome.Epiphany.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop', 'gimp.desktop', 'discord.desktop', 'steam.desktop']
     '';
 
     extraGSettingsOverridePackages = [
-      pkgs.gsettings-desktop-schemas # for org.gnome.desktop
-      pkgs.gnome.gnome-shell # for org.gnome.shell
+      pkgs.gsettings-desktop-schemas
+      pkgs.gnome.gnome-shell
+      pkgs.gnome.gnome-settings-daemon
       ];
     };
-    
+
+services.xserver.desktopManager.gnome3 = {
+    extraGSettingsOverridePackages = with pkgs; [ gnome3.gnome-settings-daemon ];
+    extraGSettingsOverrides = ''
+
+
+
+
+
 ################
 ### PRINTING ###
 ################  
