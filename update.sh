f1839acc62d826e5e6f8e7c76b026502f62be9e3
colino17 <51090962+colino17@users.noreply.github.com>
2022-04-14 10:43:01 -0300

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# PULL CONFIGS
+wget https://github.com/colino17/nixdots/archive/refs/heads/main.zip
+sleep 1
+
+# UNZIP CONFIGS
+unzip main.zip
+sleep 1
+
+# MOVE CONFIGS TO CURRENT DIRECTORY
+mv nixdots-main/* .
+sleep 1
+
+# CLEANUP
+rm -r nixdots-main
+sleep 1
+rm main.zip
