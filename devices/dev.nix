e502b82783ae6d9e0c56c04165270a004982bd2c
colino17 <51090962+colino17@users.noreply.github.com>
2022-04-14 15:09:51 -0300

    Create dev.nix

---
+++ devices/dev.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+      ./packages/base.nix
+      ./packages/gnome.nix
+      ./packages/media.nix
+      ./packages/utilities.nix
+      ./packages/web.nix
+      ./services/vpn.nix
+      ./services/games.nix
+      ./services/base.nix
+      ./services/bios.nix
+      ./home.nix
+      (import "${builtins.fetchTarball https://github.com/nix-community/home-manager/archive/master.tar.gz}/nixos")
+    ];
+}
