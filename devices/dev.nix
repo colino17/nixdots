5a23bd4db3cded54ac9e12655b7a90eeb848f79c
colino17 <51090962+colino17@users.noreply.github.com>
2022-04-14 15:16:11 -0300

    Update dev.nix

--- devices/dev.nix
+++ devices/dev.nix
@@ -10,8 +10,8 @@
       ./packages/utilities.nix
       ./packages/web.nix
       ./services/vpn.nix
+      ./services/sound.nix
       ./services/games.nix
-      ./services/base.nix
       ./services/bios.nix
       ./home.nix
       (import "${builtins.fetchTarball https://github.com/nix-community/home-manager/archive/master.tar.gz}/nixos")
