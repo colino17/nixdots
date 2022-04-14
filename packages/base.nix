d075aeba7003f1533ebca15889ca121295e386e3
colino17 <51090962+colino17@users.noreply.github.com>
2022-04-14 14:45:59 -0300

    Create base.nix

---
+++ packages/base.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, ... }:
+
+{
+  nixpkgs.config.allowUnfree = true;
+  documentation.nixos.enable = false;
+  environment.systemPackages = with pkgs; [
+    wget
+    curl
+    rsync
+    nfs-utils
+  ];
+}
