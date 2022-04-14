32c369f362be6072fef89e353e62e5624f9ba939
colino17 <51090962+colino17@users.noreply.github.com>
2022-04-14 15:13:34 -0300

    Create sound.nix

---
+++ services/sound.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, ... }:
+{
+  hardware.pulseaudio.enable = false;
+  sound.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+}
