2a7ba4d18c9c90ed4b412a0be47dacff94d65fdb
colino17 <51090962+colino17@users.noreply.github.com>
2022-04-15 00:27:59 -0300

    Update dev.nix

--- devices/dev.nix
+++ devices/dev.nix
@@ -3,17 +3,16 @@
 {
   imports =
     [
-      ./hardware-configuration.nix
-      ./packages/base.nix
-      ./packages/gnome.nix
-      ./packages/media.nix
-      ./packages/utilities.nix
-      ./packages/web.nix
-      ./services/vpn.nix
-      ./services/sound.nix
-      ./services/games.nix
-      ./services/bios.nix
-      ./home.nix
+      ../packages/base.nix
+      ../packages/gnome.nix
+      ../packages/media.nix
+      ../packages/utilities.nix
+      ../packages/web.nix
+      ../services/vpn.nix
+      ../services/sound.nix
+      ../services/games.nix
+      ../services/bios.nix
+      ../home.nix
       (import "${builtins.fetchTarball https://github.com/nix-community/home-manager/archive/master.tar.gz}/nixos")
     ];
     
