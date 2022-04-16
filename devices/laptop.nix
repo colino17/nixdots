20ef3b8c88db74a7de5068e05ae117afb1e563e8
colino17 <51090962+colino17@users.noreply.github.com>
2022-04-15 22:44:48 -0300

    Update laptop.nix

--- devices/laptop.nix
+++ devices/laptop.nix
@@ -12,13 +12,15 @@
       ../services/sound.nix
       ../services/games.nix
       ../services/uefi.nix
+      ../services/printing.nix
       ../home.nix
       (import "${builtins.fetchTarball https://github.com/nix-community/home-manager/archive/master.tar.gz}/nixos")
     ];
+    
   networking = {
     useDHCP = false;
     interfaces.enp12s0.useDHCP = true;
     interfaces.wlp3s0.useDHCP = true;
-    hostName = "chimera";
+    hostName = "manticore";
   };
 }
