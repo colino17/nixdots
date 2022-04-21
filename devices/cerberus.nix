0b8bf2f1f49053bb2893746f24df2d2fca35d0f1
colino17 <51090962+colino17@users.noreply.github.com>
2022-04-21 10:08:38 -0300

    Update and rename pi3.nix to cerberus.nix

--- devices/cerberus.nix
+++ devices/cerberus.nix
@@ -1,16 +1,5 @@
 { config, pkgs, ... }:
 
-# https://nixos.wiki/wiki/NixOS_on_ARM/Raspberry_Pi_3
-## Write image to SD Card (and USB drive??)
-## 
-# Use aarch64 image for Raspberry Pi 3 - https://hydra.nixos.org/job/nixos/release-21.11/nixos.sd_image.aarch64-linux
-# Tailscale subnets - https://tailscale.com/kb/1019/subnets/
-### "sudo tailscale up --advertise-routes=10.0.0.0/24,10.0.1.0/24" on router
-### "sudo tailscale up --accept-routes" on specific Linux clients
-
-################
-### IMPORTS ###
-################
 {
   imports =
     [
@@ -26,7 +15,6 @@
   boot.loader.generic-extlinux-compatible.enable = true;
   boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
   boot.kernel.sysctl."net.ipv6.conf.all.forwarding" = 1;
-
 
 ##################
 ### NETWORKING ###
