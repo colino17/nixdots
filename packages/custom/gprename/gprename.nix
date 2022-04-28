570579a00ddffaeaa32a6a7e85ce101ae28ba9b4
colino17 <51090962+colino17@users.noreply.github.com>
2022-04-27 22:52:22 -0300

    Update gprename.nix

--- packages/custom/gprename/gprename.nix
+++ packages/custom/gprename/gprename.nix
@@ -4,7 +4,7 @@ pkgs.stdenv.mkDerivation rec {
   pname = "gprename";
   version = "20220206";
   src = pkgs.fetchurl {
-    url = https://downloads.sourceforge.net/${pname}/${version}/${pname}-${version}.tar.bz2;
+    url = "https://downloads.sourceforge.net/${pname}/${version}/${pname}-${version}.tar.bz2";
     sha256 = "1f99947efa8d4864afff554b92e956837c7e76870681085663ddd1c8b7524e36";
   };
   
