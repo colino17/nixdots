2bfe5583b26ebb7bec5a9dae0da8028c7d0cd10c
colino17 <51090962+colino17@users.noreply.github.com>
2022-06-24 11:00:04 -0300

    Update mounts.nix

--- services/mounts.nix
+++ services/mounts.nix
@@ -8,7 +8,7 @@
   };
 
   fileSystems."/Backup" = {
-    device = "192.168.0.17:/Backup";
+    device = "192.168.0.18:/Backup";
     fsType = "nfs";
     options = [ "x-systemd.automount" "noauto" "x-systemd.mount-timeout=10" "timeo=14" "x-systemd.idle-timeout=1min" ];
   };
