2f08c045bd726892badfdfba375980873b98dbff
colino17 <51090962+colino17@users.noreply.github.com>
2022-07-05 16:22:42 -0300

    Create loki.nix

---
+++ devices/loki.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ../packages/base.nix
+      ../packages/gnome.nix
+      ../packages/media.nix
+      ../packages/utilities.nix
+      ../packages/web.nix
+      ../services/vpn.nix
+      ../services/adb.nix
+      ../services/sound.nix
+      ../services/games.nix
+      ../services/uefi.nix
+      ../services/printing.nix
+      ../services/mounts.nix
+      ../home.nix
+      (import "${builtins.fetchTarball https://github.com/nix-community/home-manager/archive/release-22.05.tar.gz}/nixos")
+    ];
+    
+  networking = {
+    hostName = "loki";
+  };
+}
