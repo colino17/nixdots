b86ff056ed159fa1ccb1d3dbfd24db4effcc1d8b
colino17 <51090962+colino17@users.noreply.github.com>
2022-07-13 13:48:11 -0300

    Update loki.nix

--- devices/loki.nix
+++ devices/loki.nix
@@ -8,12 +8,12 @@
       ../packages/media.nix
       ../packages/utilities.nix
       ../packages/web.nix
-      ../services/vpn.nix
+#      ../services/vpn.nix
       ../services/adb.nix
       ../services/sound.nix
-      ../services/games.nix
+#      ../services/games.nix
       ../services/uefi.nix
-      ../services/printing.nix
+#      ../services/printing.nix
       ../services/mounts.nix
       ../home.nix
       (import "${builtins.fetchTarball https://github.com/nix-community/home-manager/archive/release-22.05.tar.gz}/nixos")
@@ -22,4 +22,7 @@
   networking = {
     hostName = "loki";
   };
+
+
+
 }
