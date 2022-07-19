fc7ca4baa2dd0a49255f41078aff7ba97101bac6
colino17 <51090962+colino17@users.noreply.github.com>
2022-07-18 22:46:30 -0300

    Update vfio.nix

--- services/vfio.nix
+++ services/vfio.nix
@@ -12,7 +12,7 @@
         editor = false;
       };
       efi.canTouchEfiVariables = true;
-      efiSysMountPoint = "/boot/efi";
+      efi.efiSysMountPoint = "/boot/efi";
     };
     kernelParams = [ "amd_iommu=on" ];
     kernelModules = [ "vfio_virqfd" "vfio_pci" "vfio_iommu_type1" "vfio" ];
