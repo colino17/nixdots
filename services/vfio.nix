2a4ac78ace3c061baccbf185d7f7efe22f5786b6
colino17 <51090962+colino17@users.noreply.github.com>
2022-07-20 16:25:34 -0300

    Update vfio.nix

--- services/vfio.nix
+++ services/vfio.nix
@@ -36,10 +36,24 @@
       libvirtd = {
         enable = true;
         qemu = {
+          package = pkgs.qemu_kvm;
           ovmf.enable = true;
-          swtpm.enable = true;
           ovmf.package = pkgs.OVMFFull;
+          swtpm.enable = true;
+          runAsRoot = false;
         };
+      };
+    };
+
+    environment.etc = {
+      "ovmf/edk2-x86_64-secure-code.fd" = {
+        source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-x86_64-secure-code.fd";
+      };
+
+      "ovmf/edk2-i386-vars.fd" = {
+        source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-i386-vars.fd";
+        mode = "0644";
+        user = "libvirtd";
       };
     };
 
