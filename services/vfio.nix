60ffa91ab177ff8560d83ed74b1622f62ddd4a5c
colino17 <51090962+colino17@users.noreply.github.com>
2022-07-23 18:20:46 -0300

    Update vfio.nix

--- services/vfio.nix
+++ services/vfio.nix
@@ -18,15 +18,15 @@
     };
     kernelParams = [ "amd_iommu=on" ];
     kernelModules = [ "vfio_virqfd" "vfio_pci" "vfio_iommu_type1" "vfio" ];
-    extraModprobeConfig = "options vfio-pci ids=10de:2488,10de:228b,1022:57a4,1022:7901,1987:5016,8086:24fb,10ec:8161,1022:149c";
+    extraModprobeConfig = "options vfio-pci ids=10de:2488,10de:228b,10ec:8161";
     
 ### IOMMU GROUPS AND DEVICE IDS ###
 # GPU - 29 - 10de:2488,10de:228b
 # NVME - 14 - 1987:5016
 # ETHERNET - 28 - 10ec:8161
-# WIFI/BLUETOOTH - 26 - 8086:24fb
+# WIFI - 26 - 8086:24fb
 # SATA - 22 - 1022:57a4,1022:7901
-# USB - 33 - 1022:149c
+# USB/BLUETOOTH - 21 - 1022:57a4,1022:1485,1022:149c
     
   };
 
@@ -54,24 +54,9 @@
       };
     };
 
-    environment.etc = {
-      "ovmf/edk2-x86_64-secure-code.fd" = {
-        source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-x86_64-secure-code.fd";
-      };
-
-      "ovmf/edk2-i386-vars.fd" = {
-        source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-i386-vars.fd";
-        mode = "0644";
-        user = "libvirtd";
-      };
-    };
-
     environment.sessionVariables.LIBVIRT_DEFAULT_URI = [ "qemu:///system" ];
     users.users.colin = {
       extraGroups = [ "libvirtd" "kvm" "qemu-libvirtd" ];
     };
   
 }
-  
-  
-  
