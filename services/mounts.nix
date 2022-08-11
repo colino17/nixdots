0a6f50ed7fe8c8a4157f7669a47f23b3f745e8ad
colino17 <51090962+colino17@users.noreply.github.com>
2022-08-11 17:08:48 -0300

    Change unmount timeouts from 1m to 10m

--- services/mounts.nix
+++ services/mounts.nix
@@ -4,13 +4,13 @@
   fileSystems."/Storage" = {
     device = "192.168.0.17:/Storage";
     fsType = "nfs";
-    options = [ "x-systemd.automount" "noauto" "x-systemd.mount-timeout=10" "timeo=14" "x-systemd.idle-timeout=1min" ];
+    options = [ "x-systemd.automount" "noauto" "x-systemd.mount-timeout=10" "timeo=14" "x-systemd.idle-timeout=10min" ];
   };
   
   fileSystems."/CCTV" = {
     device = "192.168.0.17:/CCTV";
     fsType = "nfs";
-    options = [ "x-systemd.automount" "noauto" "x-systemd.mount-timeout=10" "timeo=14" "x-systemd.idle-timeout=1min" ];
+    options = [ "x-systemd.automount" "noauto" "x-systemd.mount-timeout=10" "timeo=14" "x-systemd.idle-timeout=10min" ];
   };
 
 }
