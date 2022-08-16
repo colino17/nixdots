82047be09a1e5886a47e96dc7e945d251ee12d9f
colino17 <51090962+colino17@users.noreply.github.com>
2022-08-16 13:53:34 -0300

    Fix typo

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -6,7 +6,7 @@
 # The exclude option excludes the temporary storage folders, Temp and Recordings, and the deleted bin, from being backed up.
 # The delete option means files deleted on the source are to be deleted on the backup as well.
 # Storage is the source folder.
-# Backup is the detsination folder.
+# Backup is the destination folder.
 # https://wiki.archlinux.org/index.php/Rsync
 
 sleep 30
