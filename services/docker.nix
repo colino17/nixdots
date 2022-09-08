9193d97b5b4b11c95b60aed140d9f2ac2a49e226
colino17 <51090962+colino17@users.noreply.github.com>
2022-09-08 20:28:28 -0300

    Add portainer and autoheal

--- services/docker.nix
+++ services/docker.nix
@@ -10,4 +10,31 @@
     autoPrune.enable = true;
   };
   
+  virtualisation.oci-containers.backend = "docker";
+  
+  virtualisation.oci-containers.containers = {
+    portainer = {
+      image = "portainer/portainer-ce:latest";
+      autoStart = true;
+      ports = [
+        "8000:8000"
+        "9443:9443"
+      ];
+      volumes = [
+        "/Configs/portainer:/data"
+        "/var/run/docker.sock:/var/run/docker.sock"
+      ];
+    };
+    autoheal = {
+      image = "willfarrell/autoheal:latest";
+      autoStart = true;
+      volumes = [
+        "/var/run/docker.sock:/var/run/docker.sock"
+      ];
+      environment = {
+        AUTOHEAL_CONTAINER_LABEL=all
+      };
+    };
+  };
+  
 }
