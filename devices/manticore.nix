a48b6983e12e2a79b79b2f21c410b7c2b4d7b569
colino17 <51090962+colino17@users.noreply.github.com>
2022-11-05 17:46:32 -0300

    Update manticore.nix

--- devices/manticore.nix
+++ devices/manticore.nix
@@ -15,6 +15,7 @@
       ../services/uefi.nix
       ../services/printing.nix
       ../services/mounts.nix
+      ../users/colin.nix
       ../home.nix
       (import "${builtins.fetchTarball https://github.com/nix-community/home-manager/archive/release-22.05.tar.gz}/nixos")
     ];
