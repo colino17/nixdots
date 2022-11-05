89d55e9fb31ce7f7c02c3f7bf3e85a18d34eeb6a
colino17 <51090962+colino17@users.noreply.github.com>
2022-11-05 17:46:57 -0300

    Update leviathan.nix

--- devices/leviathan.nix
+++ devices/leviathan.nix
@@ -15,6 +15,7 @@
       ../services/bios.nix
       ../services/printing.nix
       ../services/mounts.nix
+      ../users/colin.nix
       ../home.nix
       (import "${builtins.fetchTarball https://github.com/nix-community/home-manager/archive/release-22.05.tar.gz}/nixos")
     ];
