1b18c8676f16f114ae44bde666bbe626d3cfff9b
colino17 <51090962+colino17@users.noreply.github.com>
2022-12-03 12:18:23 -0400

    Update loki.nix

--- devices/loki.nix
+++ devices/loki.nix
@@ -14,6 +14,7 @@
       ../services/vfio.nix
       ../services/printing.nix
       ../services/mounts.nix
+      ../services/teamviewer.nix
       ../users/colin.nix
       ../home.nix
       (import "${builtins.fetchTarball https://github.com/nix-community/home-manager/archive/release-22.05.tar.gz}/nixos")
