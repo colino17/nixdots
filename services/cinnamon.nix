44ecca94f953c99efd3d87b7e99ad2735ad7bf73
colino17 <51090962+colino17@users.noreply.github.com>
2022-12-29 22:15:41 -0400

    Update cinnamon.nix

--- services/cinnamon.nix
+++ services/cinnamon.nix
@@ -8,8 +8,21 @@
   services.xserver = {
     enable = true;
     displayManager.lightdm.enable = true;
-    desktopManager.cinnamon.enable = true;
-  };
+    desktopManager.cinnamon = {
+      enable = true;
+      extraGSettingsOverrides = ''
+        [org.cinnamon.theme]
+        name = "Mint-Y-Dark-Blue"
+        [org.cinnamon.desktop.interface]
+        clock-use-24h = false
+        gtk-theme = "Mint-Y-Dark-Blue"
+        icon-theme = "Mint-Y-Dark-Blue"
+        [org.nemo.desktop]
+        home-icon-visible = false
+        computer-icon-visible = false
+        volumes-visible = false
+      '';
+    };
 
   environment.systemPackages = with pkgs; [
     gnome-console
