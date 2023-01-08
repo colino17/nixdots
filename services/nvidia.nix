f70c3c2f30bc6326b39ba3bdb55e39be1add7222
colino17 <51090962+colino17@users.noreply.github.com>
2023-01-08 19:28:54 -0400

    Update nvidia.nix

--- services/nvidia.nix
+++ services/nvidia.nix
@@ -3,4 +3,8 @@
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.opengl.enable = true;
   hardware.opengl.driSupport32Bit = true;
+  
+  virtualisation.docker = {
+    enableNvidia = true;
+  };
 }
