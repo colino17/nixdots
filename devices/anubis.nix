685490b909b7c1e589f66f70a4acd2cf9525e39b
colino17 <51090962+colino17@users.noreply.github.com>
2023-01-09 14:14:18 -0400

    Update IP

--- devices/anubis.nix
+++ devices/anubis.nix
@@ -19,10 +19,10 @@
     usePredictableInterfaceNames = false;
     interfaces.eth0.wakeOnLan.enable = true;
     interfaces.eth0.ipv4.addresses = [ {
-      address = "10.17.80.33";
+      address = "10.17.10.16";
       prefixLength = 24;
     } ];
-    defaultGateway = "10.17.80.1";
+    defaultGateway = "10.17.10.1";
     nameservers = [
       "8.8.8.8"
       "8.8.1.1"
