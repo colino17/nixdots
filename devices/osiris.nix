fd7e185e3b0a25d2fcf989d5f1c5224e7151ca59
colino17 <51090962+colino17@users.noreply.github.com>
2023-01-15 13:47:53 -0400

    Create bond network

--- devices/osiris.nix
+++ devices/osiris.nix
@@ -16,12 +16,24 @@
 ##################
 ### NETWORKING ###
 ##################
+
+
+
   networking = {
+    interfaces.enp0s25.wakeOnLan.enable = true;
     useDHCP = false;
-    usePredictableInterfaceNames = false;
-    interfaces.eth0.wakeOnLan.enable = true;
-    interfaces.eth0.ipv4.addresses = [ {
-      address = "10.17.80.19";
+    bonds.bond0 = {
+      interfaces = [
+        "enp0s25"
+        "enp6s1"
+      ];
+      driverOptions = {
+        mode = "active-backup";
+        primary = "enp0s25";
+      };
+    };
+    interfaces.bond0.ipv4.addresses = [ {
+      address = "10.17.80.17";
       prefixLength = 24;
     } ];
     defaultGateway = "10.17.80.1";
