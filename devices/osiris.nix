87a39d560a56b64f24dd5d133c8a0468b8bbaa10
colino17 <51090962+colino17@users.noreply.github.com>
2023-02-04 17:53:18 -0400

    Update osiris.nix

--- devices/osiris.nix
+++ devices/osiris.nix
@@ -30,13 +30,13 @@
       };
     };
     interfaces.bond0.ipv4.addresses = [ {
-      address = "10.17.80.17";
+      address = "10.17.10.17";
       prefixLength = 24;
     } ];
-    defaultGateway = "10.17.80.1";
+    defaultGateway = "10.17.10.1";
     nameservers = [
       "8.8.8.8"
-      "8.8.1.1"
+      "1.1.1.1"
     ];
     hostName = "osiris";
   };
