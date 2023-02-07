664c94523c3d88ce04a8430fd79d65855a7367a0
colino17 <51090962+colino17@users.noreply.github.com>
2023-02-06 20:10:14 -0400

    Update mounts.nix

--- services/mounts.nix
+++ services/mounts.nix
@@ -1,14 +1,26 @@
 { config, pkgs, ... }:
 {
 
-  fileSystems."/Storage" = {
-    device = "10.17.10.17:/Storage";
+  fileSystems."/Storage/Configs" = {
+    device = "10.17.10.17:/Configs";
     fsType = "nfs";
     options = [ "x-systemd.automount" "noauto" "x-systemd.mount-timeout=10" "timeo=14" "x-systemd.idle-timeout=60min" ];
   };
   
-  fileSystems."/CCTV" = {
-    device = "10.17.10.17:/CCTV";
+  fileSystems."/Storage/Files" = {
+    device = "10.17.10.17:/Files";
+    fsType = "nfs";
+    options = [ "x-systemd.automount" "noauto" "x-systemd.mount-timeout=10" "timeo=14" "x-systemd.idle-timeout=60min" ];
+  };
+  
+    fileSystems."/Storage/Media" = {
+    device = "10.17.10.17:/Media";
+    fsType = "nfs";
+    options = [ "x-systemd.automount" "noauto" "x-systemd.mount-timeout=10" "timeo=14" "x-systemd.idle-timeout=60min" ];
+  };
+  
+    fileSystems."/Storage/Recordings" = {
+    device = "10.17.10.17:/Recordings";
     fsType = "nfs";
     options = [ "x-systemd.automount" "noauto" "x-systemd.mount-timeout=10" "timeo=14" "x-systemd.idle-timeout=60min" ];
   };
