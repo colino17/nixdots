2676af52b198da9d67280f493f35e4182cd68ece
colino17 <51090962+colino17@users.noreply.github.com>
2023-02-27 21:32:29 -0400

    Create tlp.nix

---
+++ services/tlp.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ... }:
+{
+  services.power-profiles-daemon.enable = false;
+  services.tlp = {
+    enable = true;
+    settings = {
+      START_CHARGE_THRESH_BAT0 = 50;
+      STOP_CHARGE_THRESH_BAT0 = 85;
+      USB_AUTOSUSPEND = "0";
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    };
+}
