008611209d88260279eebb0cd7a0c60dbe611039
colino17 <51090962+colino17@users.noreply.github.com>
2023-02-28 01:41:38 -0400

    Update tlp.nix

--- services/tlp.nix
+++ services/tlp.nix
@@ -5,7 +5,7 @@
     enable = true;
     settings = {
       START_CHARGE_THRESH_BAT0 = 50;
-      STOP_CHARGE_THRESH_BAT0 = 85;
+      STOP_CHARGE_THRESH_BAT0 = 86;
       USB_AUTOSUSPEND = "0";
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
