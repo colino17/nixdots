5ba1f0450dc4cb142cb4e1d98c02825c17866176
colino17 <51090962+colino17@users.noreply.github.com>
2023-03-20 14:54:25 -0300

    Create steamos.nix

---
+++ services/steamos.nix
@@ -0,0 +1,32 @@
+{ config, pkgs, ... }:
+{
+
+## STEAM INSTALL ##
+  hardware.steam-hardware.enable = true;
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+  };
+  
+ ## PACKAGES ##
+  environment.systemPackages = with pkgs; [
+    gamescope
+    mangohud
+    pcsx2
+    retroarchFull
+  ];
+
+## GAMESCOPE SESSION ##
+  services.xserver.displayManager = {
+    session = [
+      {
+        manage = "desktop";
+        name = "gamescope";
+        start = "gamescope -e -f -h 1080 -H 1080 -r 60 -- steam --gamepadui --steamos";
+      }
+    ];
+    defaultSession = "gamescope"
+  };
+
+}
