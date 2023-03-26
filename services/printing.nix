2a04bca60dd165be98c98f13c25fcb5398e4b9cb
colino17 <51090962+colino17@users.noreply.github.com>
2023-03-26 14:47:23 -0300

    Update printing.nix

--- services/printing.nix
+++ services/printing.nix
@@ -7,14 +7,5 @@
     openFirewall = true;
     nssmdns = true;
   };
-
-  services.ipp-usb.enable=true;
-  hardware.sane = {
-    enable = true;
-    extraBackends = [ pkgs.hplipWithPlugin ];
-  };
-  environment.systemPackages = with pkgs; [
-    gnome.simple-scan
-  ];
   
 }
