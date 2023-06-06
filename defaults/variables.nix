6fe7fae55cb5c8e919668e5905af16afc0a60790
colino17 <51090962+colino17@users.noreply.github.com>
2023-06-06 15:41:30 -0300

    Update variables.nix

--- defaults/variables.nix
+++ defaults/variables.nix
@@ -1,12 +1,3 @@
-{ config, pkgs, lib, ... }:
 {
-
-  options = with lib; with types; {
-    wallpaper = mkOption { type = str; };
-  };
-  
-  config = {
-   wallpaper = "/home/colin/Pictures/Wallpapers/1080/triangles.png";
-  };
-  
+  var_wallpaper = "/home/colin/Pictures/Wallpapers/1080/triangles.png";
 }
