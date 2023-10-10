cd3694e29694e065266bc39f80a9d7d4c5897c24
colino17 <51090962+colino17@users.noreply.github.com>
2023-10-10 13:52:59 -0300

    Update btrfs.nix

--- services/btrfs.nix
+++ services/btrfs.nix
@@ -7,11 +7,39 @@
   environment.systemPackages = with pkgs; [
     btrfs-progs
     btrbk
+    lz4
   ];
-  
+
+#################
+### SCRUBBING ###
+#################
   services.btrfs.autoScrub = {
     enable = true;
     interval = "weekly";
   };
-  
+
+################
+### SECURITY ###
+################
+
+  security.sudo = {
+    enable = true;
+    extraRules = [{
+      commands = [
+        {
+          command = "${pkgs.coreutils-full}/bin/test";
+          options = [ "NOPASSWD" ];
+        }
+        {
+          command = "${pkgs.coreutils-full}/bin/readlink";
+          options = [ "NOPASSWD" ];
+        }
+        {
+          command = "${pkgs.btrfs-progs}/bin/btrfs";
+          options = [ "NOPASSWD" ];
+        }
+      ];
+      users = [ "btrbk" ];
+    }];
+  };
 }
