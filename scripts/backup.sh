abc08414af98ff80b64f9a96b09c7a4479fe8db3
colino17 <51090962+colino17@users.noreply.github.com>
2023-10-13 18:15:22 -0300

    Update backup.sh
    BTRFS Send/Receive

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,33 +1,79 @@
-#!/bin/bash
+#! /bin/bash
 
-# MODIFIED COMMAND
-# The aAX option transfers in archive mode which ensures that symbolic links, permissions and attributes are preserved.
-# The P option gives you a progress bar and allows you to resume interrupted transfers.
-# The exclude option excludes the temporary storage folders, Temp and Recordings, and the deleted bin, from being backed up.
-# The delete option means files deleted on the source are to be deleted on the backup as well.
-# Storage is the source folder.
-# Backup is the destination folder.
-# https://wiki.archlinux.org/index.php/Rsync
+coloron='\033[30;46;5;1m'
+coloroff='\033[0m'
 
-sleep 30
+set -e
 
-echo "Server Backup will begin in 60 seconds. Press CTRL-C to cancel this operation."
+##################
+## BACKUP FILES ##
+##################
 
-sleep 60
+# CREATE NEW SNAPSHOT
+echo -e "${coloron}Creating new FILES snapshot...${coloroff}"
+btrfs subvolume snapshot -r /Storage/Files /Storage/Snapshots/Files_New
+# SEND INCREMENTAL BACKUP
+echo -e "${coloron}Sending new FILES snapshot to KHONSU...${coloroff}"
+btrfs send -p /Storage/Snapshots/Files /Storage/Snapshots/Files_New | ssh root@khonsu btrfs receive -v /Backup
+# DELETE PREVIOUS SNAPSHOT
+echo -e "${coloron}Deleting previous FILES snapshot from OSIRIS...${coloroff}"
+btrfs subvolume delete /Storage/Snapshots/Files
+# DELETE PREVIOUS BACKUP
+echo -e "${coloron}Deleting previous FILES snapshot from KHONSU...${coloroff}"
+ssh root@khonsu btrfs subvolume delete /Backup/Files
+# RENAME PREVIOUS SNAPSHOT
+echo -e "${coloron}Renaming new FILES snapshot on OSIRIS...${coloroff}"
+mv /Storage/Snapshots/Files_New /Storage/Snapshots/Files
+# RENAME NEW BACKUP
+echo -e "${coloron}Renaming new FILES snapshot on KHONSU...${coloroff}"
+ssh root@khonsu mv /Backup/Files_New /Backup/Files
 
-if mountpoint -q /Storage 
-then
-   echo "Server Backup is starting..."
-   sleep 5
-   rsync -aAXP --delete --exclude={"Temp","Recordings","lost+found",".Trash-1000"} /Storage/ /Backup/
-else
-   echo "Storage Server is not available. Server Backup has been aborted."
-fi
+##################
+## BACKUP MEDIA ##
+##################
+# CREATE NEW SNAPSHOT
+echo -e "${coloron}Creating new MEDIA snapshot...${coloroff}"
+btrfs subvolume snapshot -r /Storage/Media /Storage/Snapshots/Media_New
+# SEND INCREMENTAL BACKUP
+echo -e "${coloron}Sending new MEDIA snapshot to KHONSU...${coloroff}"
+btrfs send -p /Storage/Snapshots/Media /Storage/Snapshots/Media_New | ssh root@khonsu btrfs receive -v /Backup
+# DELETE PREVIOUS SNAPSHOT
+echo -e "${coloron}Deleting previous MEDIA snapshot from OSIRIS...${coloroff}"
+btrfs subvolume delete /Storage/Snapshots/Media
+# DELETE PREVIOUS BACKUP
+echo -e "${coloron}Deleting previous MEDIA snapshot from KHONSU...${coloroff}"
+ssh root@khonsu btrfs subvolume delete /Backup/Media
+# RENAME PREVIOUS SNAPSHOT
+echo -e "${coloron}Renaming new MEDIA snapshot on OSIRIS...${coloroff}"
+mv /Storage/Snapshots/Media_New /Storage/Snapshots/Media
+# RENAME NEW BACKUP
+echo -e "${coloron}Renaming new MEDIA snapshot on KHONSU...${coloroff}"
+ssh root@khonsu mv /Backup/Media_New /Backup/Media
 
-sleep 30
+####################
+## BACKUP CONFIGS ##
+####################
+# CREATE NEW SNAPSHOT
+echo -e "${coloron}Creating new CONFIGS snapshot...${coloroff}"
+btrfs subvolume snapshot -r /Storage/Configs /.snapshots/Configs_New
+# SEND INCREMENTAL BACKUP
+echo -e "${coloron}Sending new CONFIGS snapshot to KHONSU...${coloroff}"
+btrfs send -p /.snapshots/Configs /.snapshots/Configs_New | ssh root@khonsu btrfs receive -v /Backup
+# DELETE PREVIOUS SNAPSHOT
+echo -e "${coloron}Deleting previous CONFIGS snapshot from OSIRIS...${coloroff}"
+btrfs subvolume delete /.snapshots/Configs
+# DELETE PREVIOUS BACKUP
+echo -e "${coloron}Deleting previous CONFIGS snapshot from KHONSU...${coloroff}"
+ssh root@khonsu btrfs subvolume delete /Backup/Configs
+# RENAME PREVIOUS SNAPSHOT
+echo -e "${coloron}Renaming new CONFIGS snapshot on OSIRIS...${coloroff}"
+mv /.snapshots/Configs_New /.snapshots/Configs
+# RENAME NEW BACKUP
+echo -e "${coloron}Renaming new CONFIGS snapshot on KHONSU...${coloroff}"
+ssh root@khonsu mv /Backup/Configs_New /Backup/Configs
 
-echo "Server Backup has completed. Server will shutdown in 60 seconds. Press CTRL-C to cancel this operation."
-
-sleep 60
-
-shutdown now
+#####################
+## POWEROFF KHONSU ##
+#####################
+echo -e "${coloron}Shutting Down KHONSU...${coloroff}"
+ssh root@khonsu poweroff
