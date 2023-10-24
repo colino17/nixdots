54d07141d478cd7bad73dc4b872ff7d71b6d2548
colino17 <51090962+colino17@users.noreply.github.com>
2023-10-23 22:20:34 -0300

    Update coral.nix

--- services/coral.nix
+++ services/coral.nix
@@ -1,7 +1,11 @@
 { config, pkgs, ... }:
 
-let libedgetpu = config.boot.kernelPackages.callPackage /etc/nixos/packages/coral.nix {}; in
+let 
+  libedgetpu = config.boot.kernelPackages.callPackage /etc/nixos/packages/libedgetpu.nix {}; 
+  gasket = config.boot.kernelPackages.callPackage /etc/nixos/packages/gasket.nix {};
+in
 {
   services.udev.packages = [ libedgetpu ];                                                                                                                                                                                              
   users.groups.plugdev = {};  
+  boot.extraModulePackages = [ gasket ];
 }
