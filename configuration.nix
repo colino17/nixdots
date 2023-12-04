305c003abbb86422a20106c840362cf95af750e4
colino17 <51090962+colino17@users.noreply.github.com>
2023-12-03 22:55:31 -0400

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -8,14 +8,12 @@
 #      ./devices/dadmachine.nix
 #      ./devices/loki.nix
 #      ./devices/manticore.nix
-#      ./devices/leviathan.nix
 #      ./devices/eris.nix
 # Servers
 #      ./devices/osiris.nix
 #      ./devices/anubis.nix
-#      ./devices/isis.nix
 #      ./devices/khonsu.nix
-#     ./devices/khepri.nix
+#      ./devices/khepri.nix
     ];
 
 ################
