89d930cdb77ab9fc8016ab91e388ce8093f9a72d
colino17 <51090962+colino17@users.noreply.github.com>
2023-12-05 22:39:22 -0400

    Update backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -12,21 +12,30 @@ set -e
 # CREATE NEW SNAPSHOT
 echo -e "${coloron}Creating new FILES snapshot...${coloroff}"
 btrfs subvolume snapshot -r /Storage/Files /Storage/Snapshots/Files_New
-# SEND INCREMENTAL BACKUP
+# SEND INCREMENTAL BACKUP TO KHONSU
 echo -e "${coloron}Sending new FILES snapshot to KHONSU...${coloroff}"
 btrfs send -p /Storage/Snapshots/Files /Storage/Snapshots/Files_New | ssh root@khonsu btrfs receive -v /Backup
+# SEND INCREMENTAL BACKUP TO KHEPRI
+echo -e "${coloron}Sending new FILES snapshot to KHEPRI...${coloroff}"
+btrfs send -p /Storage/Snapshots/Files /Storage/Snapshots/Files_New | ssh root@khepri btrfs receive -v /Backup
 # DELETE PREVIOUS SNAPSHOT
 echo -e "${coloron}Deleting previous FILES snapshot from OSIRIS...${coloroff}"
 btrfs subvolume delete /Storage/Snapshots/Files
-# DELETE PREVIOUS BACKUP
+# DELETE PREVIOUS BACKUP FROM KHONSU
 echo -e "${coloron}Deleting previous FILES snapshot from KHONSU...${coloroff}"
 ssh root@khonsu btrfs subvolume delete /Backup/Files
+# DELETE PREVIOUS BACKUP FROM KHEPRI
+echo -e "${coloron}Deleting previous FILES snapshot from KHEPRI...${coloroff}"
+ssh root@khepri btrfs subvolume delete /Backup/Files
 # RENAME PREVIOUS SNAPSHOT
 echo -e "${coloron}Renaming new FILES snapshot on OSIRIS...${coloroff}"
 mv /Storage/Snapshots/Files_New /Storage/Snapshots/Files
-# RENAME NEW BACKUP
+# RENAME NEW BACKUP ON KHONSU
 echo -e "${coloron}Renaming new FILES snapshot on KHONSU...${coloroff}"
 ssh root@khonsu mv /Backup/Files_New /Backup/Files
+# RENAME NEW BACKUP ON KHEPRI
+echo -e "${coloron}Renaming new FILES snapshot on KHEPRI...${coloroff}"
+ssh root@khepri mv /Backup/Files_New /Backup/Files
 
 ##################
 ## BACKUP MEDIA ##
@@ -34,21 +43,30 @@ ssh root@khonsu mv /Backup/Files_New /Backup/Files
 # CREATE NEW SNAPSHOT
 echo -e "${coloron}Creating new MEDIA snapshot...${coloroff}"
 btrfs subvolume snapshot -r /Storage/Media /Storage/Snapshots/Media_New
-# SEND INCREMENTAL BACKUP
+# SEND INCREMENTAL BACKUP TO KHONSU
 echo -e "${coloron}Sending new MEDIA snapshot to KHONSU...${coloroff}"
 btrfs send -p /Storage/Snapshots/Media /Storage/Snapshots/Media_New | ssh root@khonsu btrfs receive -v /Backup
+# SEND INCREMENTAL BACKUP TO KHEPRI
+echo -e "${coloron}Sending new MEDIA snapshot to KHEPRI...${coloroff}"
+btrfs send -p /Storage/Snapshots/Media /Storage/Snapshots/Media_New | ssh root@khepri btrfs receive -v /Backup
 # DELETE PREVIOUS SNAPSHOT
 echo -e "${coloron}Deleting previous MEDIA snapshot from OSIRIS...${coloroff}"
 btrfs subvolume delete /Storage/Snapshots/Media
-# DELETE PREVIOUS BACKUP
+# DELETE PREVIOUS BACKUP FROM KHONSU
 echo -e "${coloron}Deleting previous MEDIA snapshot from KHONSU...${coloroff}"
 ssh root@khonsu btrfs subvolume delete /Backup/Media
+# DELETE PREVIOUS BACKUP FROM KHEPRI
+echo -e "${coloron}Deleting previous MEDIA snapshot from KHEPRI...${coloroff}"
+ssh root@khepri btrfs subvolume delete /Backup/Media
 # RENAME PREVIOUS SNAPSHOT
 echo -e "${coloron}Renaming new MEDIA snapshot on OSIRIS...${coloroff}"
 mv /Storage/Snapshots/Media_New /Storage/Snapshots/Media
-# RENAME NEW BACKUP
+# RENAME NEW BACKUP ON KHONSU
 echo -e "${coloron}Renaming new MEDIA snapshot on KHONSU...${coloroff}"
 ssh root@khonsu mv /Backup/Media_New /Backup/Media
+# RENAME NEW BACKUP ON KHEPRI
+echo -e "${coloron}Renaming new MEDIA snapshot on KHEPRI...${coloroff}"
+ssh root@khepri mv /Backup/Media_New /Backup/Media
 
 ####################
 ## BACKUP CONFIGS ##
@@ -56,24 +74,36 @@ ssh root@khonsu mv /Backup/Media_New /Backup/Media
 # CREATE NEW SNAPSHOT
 echo -e "${coloron}Creating new CONFIGS snapshot...${coloroff}"
 btrfs subvolume snapshot -r /Storage/Configs /.snapshots/Configs_New
-# SEND INCREMENTAL BACKUP
+# SEND INCREMENTAL BACKUP TO KHONSU
 echo -e "${coloron}Sending new CONFIGS snapshot to KHONSU...${coloroff}"
 btrfs send -p /.snapshots/Configs /.snapshots/Configs_New | ssh root@khonsu btrfs receive -v /Backup
+# SEND INCREMENTAL BACKUP TO KHEPRI
+echo -e "${coloron}Sending new CONFIGS snapshot to KHEPRI...${coloroff}"
+btrfs send -p /.snapshots/Configs /.snapshots/Configs_New | ssh root@khepri btrfs receive -v /Backup
 # DELETE PREVIOUS SNAPSHOT
 echo -e "${coloron}Deleting previous CONFIGS snapshot from OSIRIS...${coloroff}"
 btrfs subvolume delete /.snapshots/Configs
-# DELETE PREVIOUS BACKUP
+# DELETE PREVIOUS BACKUP FROM KHONSU
 echo -e "${coloron}Deleting previous CONFIGS snapshot from KHONSU...${coloroff}"
 ssh root@khonsu btrfs subvolume delete /Backup/Configs
+# DELETE PREVIOUS BACKUP FROM KHEPRI
+echo -e "${coloron}Deleting previous CONFIGS snapshot from KHEPRI...${coloroff}"
+ssh root@khepri btrfs subvolume delete /Backup/Configs
 # RENAME PREVIOUS SNAPSHOT
 echo -e "${coloron}Renaming new CONFIGS snapshot on OSIRIS...${coloroff}"
 mv /.snapshots/Configs_New /.snapshots/Configs
-# RENAME NEW BACKUP
+# RENAME NEW BACKUP ON KHONSU
 echo -e "${coloron}Renaming new CONFIGS snapshot on KHONSU...${coloroff}"
 ssh root@khonsu mv /Backup/Configs_New /Backup/Configs
+# RENAME NEW BACKUP ON KHEPRI
+echo -e "${coloron}Renaming new CONFIGS snapshot on KHEPRI...${coloroff}"
+ssh root@khepri mv /Backup/Configs_New /Backup/Configs
 
-#####################
-## POWEROFF KHONSU ##
-#####################
+######################
+## POWEROFF BACKUPS ##
+######################
 echo -e "${coloron}Shutting Down KHONSU...${coloroff}"
 ssh root@khonsu poweroff
+
+echo -e "${coloron}Shutting Down KHEPRI...${coloroff}"
+ssh root@khepri poweroff
