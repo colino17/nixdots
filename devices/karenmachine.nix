9faabb6678e231aca3a589c2234bf5d2b7126565
colino17 <51090962+colino17@users.noreply.github.com>
2024-02-21 20:25:08 -0400

    Create karenmachine.nix

---
+++ devices/karenmachine.nix
@@ -0,0 +1,44 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ../services/base.nix
+      ../services/cinnamon.nix
+      ../services/sound.nix
+      ../services/bios.nix
+      ../services/printing.nix
+      ../users/karen.nix
+    ];
+    
+  networking = {
+    hostName = "karenmachine";
+    nameservers = [
+      "8.8.8.8"
+      "1.1.1.1"
+    ];
+  };
+  
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "karen";
+ 
+  environment.systemPackages = with pkgs; [
+    firefox
+    gnome.aisleriot
+    space-cadet-pinball
+    gnome.quadrapassel
+    gnome.gnome-mahjongg
+    gnome.gnome-mines
+    gnome.gnome-nibbles
+    gnome.gnome-sudoku
+  ];
+
+##########################
+### VERSION AND REBOOT ###
+##########################
+  system = {
+    stateVersion = "23.11";
+    autoUpgrade.allowReboot = false;
+  };
+  
+}
