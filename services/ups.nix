df8e6132aeb67386895b368c0fff6740967c05a3
colino17 <51090962+colino17@users.noreply.github.com>
2024-06-17 10:42:28 -0300

    Update ups.nix

--- services/ups.nix
+++ services/ups.nix
@@ -1,4 +1,20 @@
 { config, pkgs, ... }:
 {
-
-}
+  power.ups = { 
+    enable = true;
+    mode = "netserver";
+    openFirewall = true;
+    ups = {
+      port = "auto";
+      driver = "usbhid-ups";
+      pollinterval = "15";
+      maxretry = "5";
+    };
+    upsd = {
+      enable = true;
+      listen = [ {
+        address = "0.0.0.0";
+      } ];
+      extraConfig = "MAXAGE 25";
+    };
+  };
