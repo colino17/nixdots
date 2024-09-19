1578e3e7aa6c5cea9a7c209dbb4d713cd2c2239d
colino17 <51090962+colino17@users.noreply.github.com>
2024-09-19 10:09:22 -0300

    Update docker-agent.nix

--- services/docker-agent.nix
+++ services/docker-agent.nix
@@ -1,32 +1,10 @@
 { config, pkgs, ... }:
 {
 
-  users.users.colin.extraGroups = [ "docker" ];
-
   networking.firewall.allowedTCPPorts = [ 2375 ];
   
   virtualisation.docker = {
-    enable = true;
-    enableOnBoot = true;
-    autoPrune.enable = true;
     listenOptions = [ "/var/run/docker.sock" "0.0.0.0:2375" ];
   };
-  
-  virtualisation.oci-containers.backend = "docker";
-  
-  virtualisation.oci-containers.containers = {
-    glances = {
-      image = "nicolargo/glances:latest-alpine";
-      autoStart = true;
-      ports = [
-        "61208:61208"
-        "61209:61209"
-      ];
-      environment = {
-        GLANCES_OPT = "-w";
-      };
-      extraOptions = [ "--pull=always" ];
-    };
-  };
-  
+
 }
