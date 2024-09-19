55a6f237bfcd9c89599faa7d4cb9c021a9cc9afc
colino17 <51090962+colino17@users.noreply.github.com>
2024-09-19 00:13:50 -0300

    Remove Portainer

--- services/docker.nix
+++ services/docker.nix
@@ -15,19 +15,6 @@ let inherit (import ../variables.nix) var_hostname; in
   virtualisation.oci-containers.backend = "docker";
   
   virtualisation.oci-containers.containers = {
-    portainer = {
-      image = "portainer/portainer-ce:latest";
-      autoStart = true;
-      ports = [
-        "8000:8000"
-        "9443:9443"
-      ];
-      volumes = [
-        "/Storage/Configs/portainer:/data"
-        "/var/run/docker.sock:/var/run/docker.sock"
-      ];
-      extraOptions = [ "--pull=always" ];
-    };
     dockge = {
       image = "louislam/dockge:latest";
       autoStart = true;
