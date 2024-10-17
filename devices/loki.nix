1a58ee85638d9f21d0029df21f713d8c4e3f965d
colino17 <51090962+colino17@users.noreply.github.com>
2024-10-17 01:18:20 -0300

    Update loki.nix

--- devices/loki.nix
+++ devices/loki.nix
@@ -25,18 +25,7 @@ let inherit (import ../variables.nix) var_hmversion; in
     ];
     
   networking = {
-    useDHCP = false;
-    usePredictableInterfaceNames = false;
     interfaces.eth0.wakeOnLan.enable = true;
-    interfaces.eth0.ipv4.addresses = [ {
-      address = "10.17.10.77";
-      prefixLength = 24;
-    } ];
-    defaultGateway = "10.17.10.1";
-    nameservers = [
-      "8.8.8.8"
-      "8.8.1.1"
-    ];
     hostName = "loki";
   };
 
