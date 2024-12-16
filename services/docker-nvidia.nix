fe577a5d1ebe3376010b663956d3949ccc9f3898
colino17 <51090962+colino17@users.noreply.github.com>
2024-12-16 00:18:53 -0400

    Freeze nvidia version

--- services/docker-nvidia.nix
+++ services/docker-nvidia.nix
@@ -2,4 +2,5 @@
 {
   users.users.colin.extraGroups = [ "video" ];
   hardware.nvidia-container-toolkit.enable = true;
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_535;
 }
