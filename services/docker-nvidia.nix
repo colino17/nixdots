815a16af609153ce60c7ce5a54dd64041a4d8628
colino17 <51090962+colino17@users.noreply.github.com>
2025-01-13 15:21:49 -0400

    Update docker-nvidia.nix

--- services/docker-nvidia.nix
+++ services/docker-nvidia.nix
@@ -2,5 +2,5 @@
 {
   users.users.colin.extraGroups = [ "video" ];
   hardware.nvidia-container-toolkit.enable = true;
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_535;
+#  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_535;
 }
