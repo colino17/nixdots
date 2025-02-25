e78559620c4ee787a2ec24e6e30f35a6d7757d37
colino17 <51090962+colino17@users.noreply.github.com>
2025-02-24 22:10:44 -0400

    Update scanning.nix

--- services/scanning.nix
+++ services/scanning.nix
@@ -1,7 +1,18 @@
 { config, pkgs, ... }:
 {
 
-  hardware.sane.enable = true;
-  hardware.sane.extraBackends = [ pkgs.sane-airscan ];
-  
+  hardware.sane. = {
+    enable = true;
+    extraBackends = [ pkgs.sane-airscan ];
+    disabledDefaultBackends = [ "escl" ];
+    openFirewall = true;
+  };
+
+  services.udev.packages = [ pkgs.sane-airscan ];
+  services.saned.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    simple-scan
+  ];
+
 }
