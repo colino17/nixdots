13e28de9eba4470d44b3994ad1730a1867250b01
colino17 <51090962+colino17@users.noreply.github.com>
2025-04-26 13:27:56 -0300

    Update manticore.nix

--- devices/manticore.nix
+++ devices/manticore.nix
@@ -1,6 +1,6 @@
 { config, pkgs, ... }:
 
-let inherit (import ../variables.nix) var_hmversion; in
+let inherit (import ../variables.nix) var_username; in
 
 {
   imports =
@@ -21,9 +21,8 @@ let inherit (import ../variables.nix) var_hmversion; in
       ../services/vpn.nix
       ../services/web.nix
       ../services/x2go.nix
-      ../users/colin.nix
+      ../users/${var_username}.nix
       ../home.nix
-      (import "${builtins.fetchTarball var_hmversion}/nixos")
     ];
     
   networking = {
