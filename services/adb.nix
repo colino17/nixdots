e6f02833da269141e5d8d4ef35ead183435bc57c
colino17 <51090962+colino17@users.noreply.github.com>
2025-04-26 12:38:44 -0300

    Update adb.nix

--- services/adb.nix
+++ services/adb.nix
@@ -1,5 +1,8 @@
 { config, pkgs, ... }:
+
+let inherit (import ../variables.nix) var_username; in
+
 {
   programs.adb.enable = true;
-  users.users.colin.extraGroups = ["adbusers"];
+  users.users.${var_username}.extraGroups = ["adbusers"];
 }
