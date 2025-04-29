3bb58531578ee724df8a702d2963f52b2e845466
colino17 <51090962+colino17@users.noreply.github.com>
2025-04-29 02:09:11 -0300

    Add MPV with ModernX UI

--- services/media.nix
+++ services/media.nix
@@ -2,12 +2,23 @@
 
 {
 
+# PACKAGES
   environment.systemPackages = with pkgs; [
     gimp
     inkscape
     google-fonts
     jellyfin-media-player
     picard
+    mpv
   ];
-  
+
+# MPV INTERFACE
+  nixpkgs.overlays = [
+    (self: super: {
+      mpv = super.mpv.override {
+        scripts = [ self.mpvScripts.modernx ];
+      };
+    })
+  ];
+
 }
