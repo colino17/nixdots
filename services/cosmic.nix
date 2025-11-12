cfffb63a13a1fb1d032053e732ac6582b4bef17c
colino17 <51090962+colino17@users.noreply.github.com>
2025-11-12 11:09:38 -0400

    Create cosmic.nix

---
+++ services/cosmic.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, ... }:
+
+{
+
+## COSMIC DESKTOP ##
+  services.displayManager.cosmic-greeter.enable = true;
+  services.desktopManager.cosmic.enable = true;
+
+## EXCLUSIONS ##
+  services.desktopManager.cosmic.showExcludedPkgsWarning = false;
+  environment.cosmic.excludePackages = with pkgs; [
+    cosmic-edit
+  ];
+
+}
