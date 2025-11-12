80272a3e7d23c7cbe38bc14c830419368525890c
colino17 <51090962+colino17@users.noreply.github.com>
2025-11-11 20:49:31 -0400

    Update ide.nix

--- services/ide.nix
+++ services/ide.nix
@@ -11,43 +11,33 @@ let inherit (import ../variables.nix) var_username var_stateversion; in
   home-manager.users.${var_username} = { config, ... }: {
     programs.zed-editor.extensions = [ "kotlin" "nix" "github-dark-default" ];
     programs.zed-editor.userSettings = {
-
-        "agent": {
-          "model_parameters": [];
-          "default_model": {
-            "provider": "ollama";
-            "model": "qwen2.5-coder:14b";
-          };
-          "version": "2";
-        };
-
-        "ui_font_size": 16;
-
-        "buffer_font_size": 16;
-
-        "theme": {
-          "mode": "system";
-          "light": "One Light";
-          "dark": "GitHub Dark Default";
-        };
-
-        "language_models": {
-          "ollama": {
-            "api_url": "http://10.17.10.17:11434;
+        agent = {
+          version = 2;
+          default_model = {
+            provider = ollama;
           };
         };
-
-        "title_bar": {
-          "show_branch_icon": false;
-          "show_branch_name": true;
-          "show_project_items": true;
-          "show_onboarding_banner": true;
-          "show_user_picture": false;
-          "show_sign_in": false;
-          "show_menus": true;
+        ui_font_size = 16;
+        buffer_font_size = 16;
+        theme = {
+          mode = dark;
+          dark = GitHub Dark Default;
         };
-
-      }
+        language_models = {
+          ollama = {
+            api_url = http://10.17.10.17:11434;
+          };
+        };
+        title_bar = {
+          show_branch_icon = false;
+          show_branch_name = true;
+          show_project_items = true;
+          show_onboarding_banner = true;
+          show_user_picture = false;
+          show_sign_in = false;
+          show_menus = true;
+        };
+      };
   };
 
 }
