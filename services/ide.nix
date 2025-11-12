2af3df36a545e896a65cdcf474e13ce0714b3518
colino17 <51090962+colino17@users.noreply.github.com>
2025-11-11 20:08:00 -0400

    Create ide.nix

---
+++ services/ide.nix
@@ -0,0 +1,54 @@
+{ config, pkgs, ... }:
+
+let inherit (import ../variables.nix) var_username var_stateversion; in
+
+{
+  environment.systemPackages = with pkgs; [
+    zed-editor
+  ];
+
+## ZED SETTINGS ##
+  home-manager.users.${var_username} = { config, ... }: {
+    home.file.".config/zed/settings.json".text = ''
+      {
+
+        "agent": {
+          "model_parameters": [],
+          "default_model": {
+            "provider": "ollama",
+            "model": "qwen2.5-coder:14b"
+          },
+          "version": "2"
+        },
+
+        "ui_font_size": 16,
+
+        "buffer_font_size": 16,
+
+        "theme": {
+          "mode": "system",
+          "light": "One Light",
+          "dark": "GitHub Dark Default"
+        },
+
+        "language_models": {
+          "ollama": {
+            "api_url": "http://10.17.10.17:11434"
+          }
+        }
+
+        "title_bar": {
+          "show_branch_icon": false,
+          "show_branch_name": true,
+          "show_project_items": true,
+          "show_onboarding_banner": true,
+          "show_user_picture": false,
+          "show_sign_in": false,
+          "show_menus": true
+        }
+
+      }
+    '';
+  };
+
+}
