99659310f687bac28560396cdf5e92cc17c6da54
colino17 <51090962+colino17@users.noreply.github.com>
2025-11-17 11:48:12 -0400

    Update ide.nix

--- services/ide.nix
+++ services/ide.nix
@@ -10,20 +10,21 @@ let inherit (import ../variables.nix) var_username; in
 ## ZED SETTINGS ##
   home-manager.users.${var_username} = { config, ... }: {
     programs.zed-editor.enable = true;
-    programs.zed-editor.extensions = [ "kotlin" "nix" "github-dark-default" "nstlgy-dark" ];
+    programs.zed-editor.extensions = [ "nix" "dark-pop-ui" ];
     programs.zed-editor.userSettings = {
         agent = {
           version = "2";
           default_model = {
             provider = "ollama";
+            model = "qwen2.5:7b";
           };
         };
-        ui_font_size = 16;
-        buffer_font_size = 16;
+        ui_font_size = 14;
+        buffer_font_size = 14;
         theme = {
           mode = "system";
-          dark = "Nstlgy Dark";
-          light = "Nstlgy Dark";
+          dark = "Dark Pop UI";
+          light = "Dark Pop UI";
         };
         language_models = {
           ollama = {
@@ -32,12 +33,12 @@ let inherit (import ../variables.nix) var_username; in
         };
         title_bar = {
           show_branch_icon = false;
-          show_branch_name = true;
+          show_menus = true;
+          show_branch_name = false;
           show_project_items = true;
           show_onboarding_banner = false;
           show_user_picture = false;
           show_sign_in = false;
-          show_menus = true;
         };
       };
   };
