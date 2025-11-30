e472679c21a8f61bf7207dd0c98e35a4160a6a68
colino17 <51090962+colino17@users.noreply.github.com>
2025-11-30 19:26:31 -0400

    Update variables.nix

--- defaults/variables.nix
+++ defaults/variables.nix
@@ -1,7 +1,7 @@
 {
   var_wallpaper = "/home/colin/Pictures/Wallpapers/1080/triangles.png";
-  var_hmversion = "https://github.com/nix-community/home-manager/archive/release-24.05.tar.gz";
+  var_hmversion = "https://github.com/nix-community/home-manager/archive/release-25.11.tar.gz";
   var_hostname = "";
   var_username = "colin";
-  var_stateversion = "25.05";
+  var_stateversion = "25.11";
 }
