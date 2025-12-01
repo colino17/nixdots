5e8af29a664373b642a0432b13ee221366cee3fe
colino17 <51090962+colino17@users.noreply.github.com>
2025-11-30 20:28:42 -0400

    Update cosmic.nix

--- services/cosmic.nix
+++ services/cosmic.nix
@@ -10,11 +10,13 @@
   services.desktopManager.cosmic.showExcludedPkgsWarning = false;
   environment.cosmic.excludePackages = with pkgs; [
     cosmic-edit
+    cosmic-player
   ];
 
 ## PACKAGES ##
   environment.systemPackages = with pkgs; [
     numix-icon-theme-circle
+    clapper
   ];
 
 }
