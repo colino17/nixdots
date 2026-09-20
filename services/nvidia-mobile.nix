fd532b0e0cc23c9a1e388f0741e0be28946417fe
colino17 <51090962+colino17@users.noreply.github.com>
2026-09-20 11:22:36 -0300

    Refactor NVIDIA mobile configuration for gaming
    Updated NVIDIA and modesetting configurations for gaming and default setups.

--- services/nvidia-mobile.nix
+++ services/nvidia-mobile.nix
@@ -1,16 +1,31 @@
 { config, pkgs, ... }:
 
 {
-  services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia.open = false;
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
+
+# DEFAULT CONFIGURATION
+  services.xserver.videoDrivers = [ "modesetting" ];
+  boot.initrd.kernelModules = [ "amdgpu" ];
+  boot.blacklistedKernelModules = [ "nvidia" "nvidia_uvm" "nvidia_modeset" "nvidia_drm" "nouveau" ];
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
+  '';
+
+# GAMING CONFIGURATION
+  specialisation."Gaming".configuration = {
+    services.xserver.videoDrivers = [ "nvidia" ];
+    boot.blacklistedKernelModules = [ "nouveau" ];
+    services.udev.extraRules = pkgs.lib.mkForce "";
+    hardware.nvidia = {
+      modesetting.enable = true;
+      open = false; 
+      nvidiaSettings = true;
+      prime = {
+        sync.enable = true; # All apps run on NVIDIA by default
+        amdgpuBusId = "PCI:4:0:0";  
+        nvidiaBusId = "PCI:1:0:0";  
+      };
+    };
   };
-  services.switcherooControl.enable = true;
-  hardware.nvidia.prime = {
-    offload.enable = true;
-    amdgpuBusId = "PCI:4:00:0";
-    nvidiaBusId = "PCI:1:00:0";
-  };
+
 }
