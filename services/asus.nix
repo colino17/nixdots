fa804c1686aad7e44dba035404eec4d19efcd7ab
colino17 <51090962+colino17@users.noreply.github.com>
2026-09-21 17:19:48 -0300

    Add ASUS service and modify gaming configuration
    Added ASUS service configuration and updated gaming settings.

--- services/asus.nix
+++ services/asus.nix
@@ -3,7 +3,15 @@
 {
 
 # DEFAULT CONFIGURATION
+  services.asusd.enable = true;
+  environment.etc."asusd/.keep".text = "";
+  services.power-profiles-daemon.enable = true;
+  environment.systemPackages = with pkgs; [
+    asusctl
+    brightnessctl
+  ];
   services.xserver.videoDrivers = [ "modesetting" ];
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.initrd.kernelModules = [ "amdgpu" ];
   boot.blacklistedKernelModules = [ "nvidia" "nvidia_uvm" "nvidia_modeset" "nvidia_drm" "nouveau" ];
   services.udev.extraRules = ''
@@ -11,8 +19,9 @@
     ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
   '';
 
-# GAMING CONFIGURATION
+# GAMING CONFIGURATION (USES OLDER KERNEL UNTIL NVIDIA DRIVERS UPDATE TO ACCOUNT FOR KERNEL 7.2 CHANGES)
   specialisation."Gaming".configuration = {
+    boot.kernelPackages = pkgs.lib.mkForce pkgs.linuxPackages;
     services.xserver.videoDrivers = [ "nvidia" ];
     boot.blacklistedKernelModules = [ "nouveau" ];
     services.udev.extraRules = pkgs.lib.mkForce "";
