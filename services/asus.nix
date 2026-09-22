f618e7a8f0ee090460b2a5289e5e445572645436
colino17 <51090962+colino17@users.noreply.github.com>
2026-09-22 13:30:36 -0300

    Refactor asus.nix for integrated and discrete graphics

--- services/asus.nix
+++ services/asus.nix
@@ -1,38 +1,71 @@
-{ config, pkgs, ... }:
+{ lib, config, pkgs, ... }:
+
+let inherit (import ../variables.nix) var_username; in
 
 {
 
-# DEFAULT CONFIGURATION
-  services.asusd.enable = true;
-  environment.etc."asusd/.keep".text = "";
-  services.power-profiles-daemon.enable = true;
-  environment.systemPackages = with pkgs; [
-    asusctl
-    brightnessctl
-  ];
-  services.xserver.videoDrivers = [ "modesetting" ];
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.initrd.kernelModules = [ "amdgpu" ];
-  boot.blacklistedKernelModules = [ "nvidia" "nvidia_uvm" "nvidia_modeset" "nvidia_drm" "nouveau" ];
-  services.udev.extraRules = ''
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
-  '';
+## DEFAULT CONFIGURATION (INTEGRATED GRAPHICS)
+  users.users.${var_username}.extraGroups = [ "video" "render" ];
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+    initrd.kernelModules = [ "amdgpu" ];
+    blacklistedKernelModules = [ "nvidia" "nvidia_uvm" "nvidia_modeset" "nvidia_drm" "nouveau" ];
+  };
+  services = {
+    asusd.enable = true;
+    power-profiles-daemon.enable = true;
+    xserver.videoDrivers = [ "modesetting" ];
+    udev.extraRules = ''
+      ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+      ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
+    '';
+  };
+  environment = {
+    etc."asusd/.keep".text = "";
+    systemPackages = with pkgs; [
+      asusctl
+      brightnessctl
+    ];
+  };
 
-# GAMING CONFIGURATION (USES OLDER KERNEL UNTIL NVIDIA DRIVERS UPDATE TO ACCOUNT FOR KERNEL 7.2 CHANGES)
+## GAMING CONFIGURATION (DISCRETE GRAPHICS)
+## USES AN OLDER KERNEL UNTIL NVIDIA DRIVERS WORK PROPERLY WITH KERNEL 7.2
   specialisation."Gaming".configuration = {
-    boot.kernelPackages = pkgs.lib.mkForce pkgs.linuxPackages;
-    services.xserver.videoDrivers = [ "nvidia" ];
-    boot.blacklistedKernelModules = [ "nouveau" ];
-    services.udev.extraRules = pkgs.lib.mkForce "";
-    hardware.nvidia = {
-      modesetting.enable = true;
-      open = false; 
-      nvidiaSettings = true;
-      prime = {
-        sync.enable = true; # All apps run on NVIDIA by default
-        amdgpuBusId = "PCI:4:0:0";  
-        nvidiaBusId = "PCI:1:0:0";  
+    boot = {
+      kernelPackages = lib.mkForce pkgs.linuxPackages;
+      blacklistedKernelModules = lib.mkForce [ "nouveau" ];
+    };
+    services = {
+      xserver.videoDrivers = lib.mkForce [ "nvidia" ];
+      udev.extraRules = lib.mkForce "";
+    };
+    hardware = {
+      graphics = {
+        enable = true;
+        enable32Bit = true;
+      };
+      nvidia = {
+        modesetting.enable = true;
+        open = false;
+        nvidiaSettings = true;
+        nvidiaPersistenced = true;
+        powerManagement.enable = true;
+        prime = {
+          offload.enable = true;
+          offload.enableOffloadCmd = true;
+          amdgpuBusId = "PCI:4:0:0";
+          nvidiaBusId = "PCI:1:0:0";
+        };
+      };
+    };
+    programs.steam = {
+      enable = true;
+      package = pkgs.steam.override {
+        extraEnv = {
+          __NV_PRIME_RENDER_OFFLOAD = "1";
+          __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+          __VK_LAYER_NV_optimus = "NVIDIA_only";
+        };
       };
     };
   };
